70ea6d49cc118853981fa5601efb32f125b58c3a
bagustris <bagustris@yahoo.com>
2018-08-19 18:22:20 +0900

    add quickshort

---
+++ code/quikshort.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksort(left) + middle + quicksort(right)
+
+
+def main():
+    print(quicksort([3, 6, 8, 10, 1, 2, 1]))
+
+
+if __name__ == "__main__":
+    main()
