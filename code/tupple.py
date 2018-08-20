f1a4c249fabb73dd650b30138e75e7b8e9c950bc
bagustris <bagustris@yahoo.com>
2018-08-20 15:47:24 +0900

    mod tupple.py

--- code/tupple.py
+++ code/tupple.py
@@ -1,7 +1,21 @@
 # demo tuples in python
+# Tuple is similar to list
+# but can be used as key in dict
 
+# create dictionary with tuple key
 d = {(x, x + 1): x for x in range(10)}
+
+# create tuple
 t = (5, 6)
+
+# print type of tuple
 print(type(t))
+
+# print "5": key only
 print(d[t])
+
+# print "1"
 print(d[(1, 2)])
+
+/bin/bash: q: command not found
+print(d[(3, 6)])
